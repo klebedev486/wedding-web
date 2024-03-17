0700abd2f9e531d74cf0795b4a99673997c9fb3f
klebedev486 <137430496+klebedev486@users.noreply.github.com>
2024-03-17 17:11:21 -0500

    Add files via upload

--- countdown.js
+++ countdown.js
@@ -1,9 +1,9 @@
-const targetDate = new Date(2024, 7, 13); // July 1, 2024
+const targetDate = new Date(2024, 7, 13); 
 
 // Function to update the countdown timer
 function updateCountdown() {
     // Get the current date and time
-    const currentDate = new Date();
+    const currentDate = new Date(2024, 3, 17);
 
     // Calculate the difference in milliseconds between the target date and the current date
     const difference = targetDate - currentDate;
